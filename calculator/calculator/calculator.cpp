b5d8e633f13f9890a8fc83342ca9e24ef9a3913b
timofey cheban <timofeycheban64@gmail.com>
2020-11-12 10:34:06 +0300

    add

---
+++ calculator/calculator/calculator.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, r;
+	char q;
+	cout << "enter the first number: " << endl;
+	cin >> a;
+	cout << "enter the second number: " << endl;
+	cin >> b;
+	cout << "action: ";
+	cin >> q;
+
+	switch (q) {
+
+	case '+':
+		r = a + b;
+		cout << "Result: " << r << endl;
+		break;
+
+	case '-':
+		r = a - b;
+		cout << "Result: " << r << endl;
+		break;
+
+	case '*':
+		r = a * b;
+		cout << "Result: " << r << endl;
+		break;
+
+	case '/':
+		r = a / b;
+		cout << "Result: " << r << endl;
+		break;
+
+	case '%':
+		r = a % b;
+		cout << "Result: " << r << endl;
+		break;
+
+	default:    cout << "incorrect value." << endl;
+
+	}
+
+
+	return 0;
+}
